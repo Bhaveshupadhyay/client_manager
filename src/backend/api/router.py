de9f3c27b6b03deee9e123941037190036cf4778
Bhaveshupadhyay <bhaveshupadhyay929@gmail.com>
2026-07-05 19:16:33 +0530

    fix: config file

--- src/backend/api/router.py
+++ src/backend/api/router.py
@@ -1,11 +1,10 @@
 from fastapi import APIRouter
-from v1.auth import router as auth_router
-from v1.chat import router as chat_router
-from v1.file import router as file_router
+from backend.api.v1.chat import router as chat_router
+from backend.api.v1.file import router as file_router
 
 routers = APIRouter()
 
-routers_list= [auth_router,chat_router,file_router]
+routers_list= [chat_router,file_router]
 for router in routers_list:
     router.tags = routers.tags.append("v1")
     routers.include_router(router)
--- src/backend/core/config.py
+++ src/backend/core/config.py
@@ -3,3 +3,7 @@ from pydantic_settings import BaseSettings
 
 class Config(BaseSettings):
     API_V1_STR: str = "/api/v1"
+    PROJECT_NAME: str = "My Ai Client Manager"
+
+
+config: Config = Config()
